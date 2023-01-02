7d70448940303fc6bf190ebc7d6e762791675ad8
VictoriaVMironova <86355483+VictoriaVMironova@users.noreply.github.com>
2023-01-02 13:59:36 +0100

    Delete Lectures_supp.R

--- Lectures_supp.R
+++
@@ -1,8 +0,0 @@
-install.packages('xaringan')
-
-#The main R Markdown output format in this package is moon_reader().
-#See the R help page
-?xaringan::moon_reader
-
-#help on the xaringan presentations:
-# https://bookdown.org/yihui/rmarkdown/xaringan-key.html

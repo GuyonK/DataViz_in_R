edabac9d40cbf0ca1f3d9c640aadf7062d8cd094
VictoriaVMironova <victoria.mironova@ru.nl>
2023-01-09 12:13:41 +0100

    Intro to GitHub file is created

--- _packages.R
+++ _packages.R
@@ -1,13 +1,11 @@
 # Install packages
 
 my_packages <- c("tidyverse", "ggplot2", "cowplot", "colorblindr", "remotes", "xaringan", "xaringanExtra")
-remotes::install_github("clauswilke/colorblindr")
-remotes::install_github("jhelvy/xaringanBuilder", force = TRUE)
-remotes::install_github('rstudio/chromote')
+
 remotes::install_github('renderthis')
 install.packages('pdftools')
 
-forceinstall.packages(my_packages)
+install.packages(my_packages)
 
 install.packages("colorspace")
 install.packages("ggforce")
@@ -15,6 +13,8 @@ install.packages("kableExtra")
 install.packages("ggpubr")
 
 # install.packages("remotes")
-
+#remotes::install_github("jhelvy/xaringanBuilder", force = TRUE)
+#remotes::install_github("clauswilke/colorblindr")
+#remotes::install_github('rstudio/chromote')
 
 

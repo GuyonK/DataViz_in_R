8502028a9e0ad36834104d411b70b4caf409e9f5
GuyonK <guyonn@hotmail.com>
2025-02-17 11:28:45 +0100

    Update _packages.R

--- _packages.R
+++ _packages.R
@@ -1,8 +1,11 @@
 # Install packages
 
 my_packages <- c("tidyverse", "ggplot2", "cowplot", "colorblindr", "remotes", "xaringan", "xaringanExtra")
+install.packages("remotes")
 
 remotes::install_github('renderthis')
+remotes::install_github("jhelvy/renderthis")
+
 install.packages('pdftools')
 
 install.packages(my_packages)
@@ -13,7 +16,7 @@ install.packages("kableExtra")
 install.packages("ggpubr")
 
 # install.packages("remotes")
-#remotes::install_github("jhelvy/xaringanBuilder", force = TRUE)
+remotes::install_github("jhelvy/xaringanBuilder", force = TRUE)
 #remotes::install_github("clauswilke/colorblindr")
 #remotes::install_github('rstudio/chromote')
 
